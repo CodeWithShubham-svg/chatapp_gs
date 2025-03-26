410d5cd9d91e9a0d2383cee14887bbcf6a9474c1
CodeWithShubham-svg <hypersect000@gmail.com>
2025-03-26 18:58:43 +0530

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,90 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { collection, doc, getDocs, getFirestore, query, setDoc, where } from 'firebase/firestore'
+import { toast } from "react-toastify";
+
+// --------- Paste Your Firebase Config File Here ---------
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADwKKiTQegjPGJIKmlRWGg_I8mk3gsIt8",
+    authDomain: "chatapp-4ffbd.firebaseapp.com",
+    projectId: "chatapp-4ffbd",
+    storageBucket: "chat-app-10482.appspot.com",
+    messagingSenderId: "965731544957",
+    appId: "1:965731544957:web:e3ce6aab4a7cc7361a2614"
+  }
+
+//   personal storagebucket-non-work: chatapp-4ffbd.firebasestorage.app
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signup = async (username, email, password) => {
+    try {
+        const usersRef = collection(db,'users')
+        const q = query(usersRef,where("username", "==" ,username.toLowerCase()))
+        const querySnapshot = await getDocs(q)
+        if(querySnapshot.docs.length>0){
+            toast.error("Username already taken")
+            return 0;
+        }
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user = res.user
+        await setDoc(doc(db,"users",user.uid), {
+            id: user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There i am using chat app",
+            lastSeen: Date.now()
+        });
+        await setDoc(doc(db,"chats",user.uid), {
+            chatsData:[]
+        });
+
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+const logout = () => {
+    signOut(auth)
+}
+
+const resetPass = async (email) => {
+    if (!email) {
+        toast.error("Enter your email")
+        return null
+    }
+    try {
+        const userRef = collection(db, "users")
+        const q = query(userRef, where("email", "==", email))
+        const querySnap = await getDocs(q)
+        if (!querySnap.empty) {
+            await sendPasswordResetEmail(auth,email)
+            toast.success("Reset Email Sent")
+        }
+        else {
+            toast.error("Email doesn't exists")
+        }
+    } catch (error) {
+        console.error(error)
+        toast.error(error.message)
+    }
+   
+}
+
+export { auth, db, login, signup, logout, resetPass};
